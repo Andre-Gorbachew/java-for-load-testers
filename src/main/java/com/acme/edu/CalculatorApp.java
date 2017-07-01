735e7d63cd4f3399616997610174722284165b71
Andre-Gorbachew <andre-gorbachew@yandex.ru>
2017-07-01 13:51:45 +0300

    01 07 home work 1) its done 2) its done log 3) its done getLog
    Signed-off-by: Andre-Gorbachew <andre-gorbachew@yandex.ru>

--- src/main/java/com/acme/edu/CalculatorApp.java
+++ src/main/java/com/acme/edu/CalculatorApp.java
@@ -1,40 +1,10 @@
 package com.acme.edu;
 
+import static com.acme.edu.calculator.*;
 import static java.lang.Double.parseDouble;
-import static java.lang.Integer.parseInt;
 
 public class CalculatorApp {
-
     public static void main(String[] args) {
-        /*String par1 = args[0];
-
-        String par2 = args[1];
-        Double per1 = Double.parseDouble(par2);
-
-        String par3 = args[2];
-        Double per2 = Double.parseDouble(par3);
-        switch (par1) {
-            case "module" : System.out.println(calculator2.module(per1)); break;
-            case "div" : System.out.println(calculator2.div(per1, per2)); break;
-            case "sub" : System.out.println(calculator2.sub(per1, per2)); break;
-            case "mul" : System.out.println(calculator2.mul(per1, per2)); break;
-            case "sum" : System.out.println(calculator2.sum(per1, per2)); break;
-        }*/
-        /*String comand = "";
-        String ag1 = "";
-        String ag2 = "";
-        int postion = 0;
-        for (String i : args){
-            if(postion++ == 0){
-                comand = i;
-            } else if (postion == 1){
-                ag1 = i;
-            }else {
-                ag2 = i;
-                postion = 0;
-            }
-
-        }*/
         String command = "";
         String arg1 = "";
         String arg2 = "";
@@ -44,6 +14,13 @@ public class CalculatorApp {
             switch (position++) {
                 case 0:
                     command = current;
+                    if (command.equals("getLog")){
+                        for(String logEntry : getLog()){
+                            System.out.println(">>> " + logEntry);
+                        }
+                        position = 0;
+                        break;
+                    }
                     break;
                 case 1:
                     arg1 = current;
@@ -52,39 +29,29 @@ public class CalculatorApp {
                     arg2 = current;
                     doCalculate(command, parseDouble(arg1), parseDouble(arg2));
                     position = 0;
+                    break;
                 }
+                default: System.out.println("Error");
             }
         }
-
     }
-
     public static void doCalculate(String operator, Double arg1, Double arg2) {
         switch (operator) {
             case "module":
-                System.out.println(calculator2.module(arg1));
+                System.out.println(module(arg1));
                 break;
             case "div":
-                stringLog(arg1 + " " + operator + " " + arg2 + " = " + calculator2.div(arg1, arg2));
+                System.out.println(arg1 + " " + operator + " " + arg2 + " = " + div(arg1, arg2));
                 break;
             case "sub":
-                stringLog(arg1 + " " + operator + " " + arg2 + " = " + calculator2.sub(arg1, arg2));
+                System.out.println(arg1 + " " + operator + " " + arg2 + " = " + sub(arg1, arg2));
                 break;
             case "mul":
-                stringLog(arg1 + " " + operator + " " + arg2 + " = " + calculator2.mul(arg1, arg2));
+                System.out.println(arg1 + " " + operator + " " + arg2 + " = " + mul(arg1, arg2));
                 break;
             case "sum":
-                stringLog(arg1 + " " + operator + " " + arg2 + " = " + calculator2.sum(arg1, arg2));
+                System.out.println(arg1 + " " + operator + " " + arg2 + " = " + sum(arg1, arg2));
                 break;
         }
-    }
-
-    public static String stringLog(String... b) {
-        String result = "";
-        for (String current : b) {
-            result = result + current;
-        }
-
-        System.out.println(result);
-        return result;
     }
 }
--- src/main/java/com/acme/edu/calculator.java
+++ src/main/java/com/acme/edu/calculator.java
@@ -1,44 +1,97 @@
 package com.acme.edu;
+public class calculator {
+    public static StringBuilder log = new StringBuilder();
 
-import java.io.File;
-
-/**
- * Created by AA.Gorbachev on 27.06.2017.
- */
-public class calculator
-{
-    private static int memory;
-
-    public void addToMemory(){
-        //region test
-
-        //sout
-        System.out.println("adssa");
-
-        byte q =0;
-        short w = 07;
-        int p = 0;
-        //long l = 9999999999999; - не работает
-        long e = 9999999999999L; // работает
-
-        float f = 0.1F; // работает
-        //float t = 0.1; не работает
-
-        double ff = -3.13e-10;
-
-        boolean rr = false;
-
-        char v = '\uABCF';
-        char vv = '\t';
-        char vvv = '\n';
-
-
-        System.out.println(v);
-        //endregion
+    public static int sum(int a, int b) {
+        if(b == 0){
+            return 0;
+        }
+        log.append(a).append(" sum ").append(b).append(" = ").append(a + b).append("\n");
+        return restriction(a + b);
+    }
+    public static double sum(double a, double b) {
+        if(b == 0){
+            return 0.;
+        }
+        log.append(a).append(" sum ").append(b).append(" = ").append(a + b).append("\n");
+        return restriction(a + b);
     }
 
-    public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+    public static int sub(int a, int b) {
+        if(b == 0){
+            return 0;
+        }
+        log.append(a).append(" sub ").append(b).append(" = ").append(a - b).append("\n");
+        return restriction(a - b);
+    }
+    public static double sub(double a, double b) {
+        if(b == 0){
+            return 0.;
+        }
+        log.append(a).append(" sub ").append(b).append(" = ").append(a - b).append("\n");
+        return restriction(a - b);
+    }
+
+    public static int mul(int a, int b) {
+        if(b == 0){
+            return 0;
+        }
+        log.append(a).append(" mul ").append(b).append(" = ").append(a * b).append("\n");
+        return restriction(a * b);
+    }
+    public static double mul(double a, double b) {
+        if(b == 0){
+            return 0.;
+        }
+        log.append(a).append(" mul ").append(b).append(" = ").append(a * b).append("\n");
+        return restriction(a * b);
+    }
+
+    public static int div(int a, int b) {
+        if(b == 0){
+            return 0;
+        }
+        log.append(a).append(" div ").append(b).append(" = ").append(a / b).append("\n");
+        return restriction(a / b);
+    }
+    public static double div(double a, double b) {
+        if(b == 0){
+            return 0.;
+        }
+        log.append(a).append(" div ").append(b).append(" = ").append(a / b).append("\n");
+        return restriction(a / b);
+    }
+    public static String[] getLog(){
+        return log.toString().split("\n");
+    }
+    public static int module(int w){
+       if(w < 0){
+           w = -w;
+       }
+       return restriction(w);
+    }
+    public static double module(double w){
+        if(w < 0){
+            w = -w;
+        }
+        return restriction(w);
+    }
+    public static double restriction(double w){
+        if(w >= -10 & w <= 10) {
+            return w;
+        }else if (w > 10){
+            return 10;
+        }else{
+            return -10;
+        }
+    }
+    public static int restriction(int w){
+        if(w >= -10 & w <= 10) {
+            return w;
+        }else if (w > 10){
+            return 10;
+        }else{
+            return -10;
+        }
     }
 }
-
